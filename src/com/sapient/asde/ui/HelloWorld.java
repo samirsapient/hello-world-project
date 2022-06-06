eb0711296e26120aaa8ad0fc19090aee8b2ee6d7
Samir Kumar <samkumar4@publicisgroupe.net>
2022-06-06 16:20:40 +0530

    Second Commit

--- src/com/sapient/asde/ui/HelloWorld.java
+++ src/com/sapient/asde/ui/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello World!");
             System.out.println("Hi, "+args[0]);
+            System.out.println("Second");
 
         }
 
